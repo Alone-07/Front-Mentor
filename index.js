477c99734ad93f408c28d1c86af7d69c9a55f066
Alone007 <email_address>
2022-01-20 00:13:10 +0530

    just finished mobile design

---
+++ index.js
@@ -0,0 +1,73 @@
+console.log("hi");
+
+function myToggle1() {
+  const hide = document.getElementById("open1");
+  const arrow = document.getElementById("one");
+  // const shows = document.querySelectorAll("p.text_show_part");
+  const shows = document.getElementById("show1");
+
+  if (hide.style.display == "none") {
+    hide.style.display = "block";
+    arrow.style.transform = "rotate(180deg)";
+    shows.style.fontWeight = "700";
+  } else {
+    hide.style.display = "none";
+    arrow.style.transform = "rotate(0deg)";
+  }
+}
+function myToggle2() {
+  const hide = document.getElementById("open2");
+  const arrow = document.getElementById("two");
+  const shows = document.getElementById("show2");
+
+  if (hide.style.display == "none") {
+    hide.style.display = "block";
+    arrow.style.transform = "rotate(180deg)";
+    shows.style.fontWeight = "700";
+  } else {
+    hide.style.display = "none";
+    arrow.style.transform = "rotate(0deg)";
+  }
+}
+function myToggle3() {
+  const hide = document.getElementById("open3");
+  const arrow = document.getElementById("three");
+  const shows = document.getElementById("show3");
+
+  if (hide.style.display == "none") {
+    hide.style.display = "block";
+    arrow.style.transform = "rotate(180deg)";
+    shows.style.fontWeight = "700";
+  } else {
+    hide.style.display = "none";
+    arrow.style.transform = "rotate(0deg)";
+  }
+}
+function myToggle4() {
+  const hide = document.getElementById("open4");
+  const arrow = document.getElementById("four");
+  const shows = document.getElementById("show4");
+
+  if (hide.style.display == "none") {
+    hide.style.display = "block";
+    arrow.style.transform = "rotate(180deg)";
+    shows.style.fontWeight = "700";
+  } else {
+    hide.style.display = "none";
+    arrow.style.transform = "rotate(0deg)";
+  }
+}
+function myToggle5() {
+  const hide = document.getElementById("open5");
+  const arrow = document.getElementById("five");
+  const shows = document.getElementById("show5");
+
+  if (hide.style.display == "none") {
+    hide.style.display = "block";
+    arrow.style.transform = "rotate(180deg)";
+    shows.style.fontWeight = "700";
+  } else {
+    hide.style.display = "none";
+    arrow.style.transform = "rotate(0deg)";
+  }
+}
