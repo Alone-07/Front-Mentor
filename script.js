3743386df4bd31fca90c793080ac033759597973
Alone007 <email_address>
2022-02-05 12:32:41 +0530

    just finished

---
+++ script.js
@@ -0,0 +1,16 @@
+const shareIcon = document.querySelector('.share');
+const toggleContainer = document.querySelector('#toggle');
+const hideShare = document.querySelector('.hiding_share');
+const displayProfile = document.querySelector('.displaying_profile');
+
+shareIcon.addEventListener('click', () => {
+
+    if (hideShare.style.opacity == 0) {
+        hideShare.style.opacity = 1;
+    } else {
+        hideShare.style.opacity = 0;
+    }
+
+})
+
+// 'hsl(217, 19%, 35%)';
