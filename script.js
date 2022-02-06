2977766b1e6c58b02626af29885ca4b00361e700
Alone007 <email_address>
2022-02-06 14:02:15 +0530

    done some javaScript

--- script.js
+++ script.js
@@ -2,17 +2,26 @@ const shareIcon = document.querySelector('.share');
 const toggleContainer = document.querySelector('#toggle');
 const hideShare = document.querySelector('.hiding_share');
 const displayProfile = document.querySelector('.displaying_profile');
-
+let customerScreen = screen.availWidth;
 shareIcon.addEventListener('click', () => {
+    console.log(customerScreen);
+    if (customerScreen < 834) { //when the screen size less than 834px t
+        if (hideShare.style.opacity == 0) {
+            hideShare.style.opacity = 1
+            toggleContainer.style.backgroundColor = 'hsl(217, 19%, 35%)'
+        } else {
+            hideShare.style.opacity = 0
+            toggleContainer.style.backgroundColor = 'white'
+        }
+    }
+    if (customerScreen > 834) {
 
-    if (hideShare.style.opacity == 0) {
-        hideShare.style.opacity = 1;
-
-    } else {
-        hideShare.style.opacity = 0;
-
+        if (hideShare.style.opacity == 0) {
+            hideShare.style.opacity = 1;
+        } else {
+            hideShare.style.opacity = 0;
+        }
     }
 })
-
 
 // 'hsl(217, 19%, 35%)';
