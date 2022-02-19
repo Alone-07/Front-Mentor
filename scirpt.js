1b921f6bf423e95a82c12a12c10e5e0fb5936c63
Alone007 <email_address>
2022-02-19 13:29:41 +0530

    added some features to pages and js file changed little bit

--- scirpt.js
+++ scirpt.js
@@ -6,28 +6,40 @@ const toggle = document.querySelector('[data-toggle]');
 const toggler = document.querySelector('[data-toggler]');
 const body = document.querySelector('body');
 const dark = 'dark';
-// const toggleBG = 'linear-gradient(to right , $toggle_grad1, $toggle_grad2)';
+const light = 'light';
+// localStorage.removeItem('mode');
+let modeOutside = localStorage.getItem('mode');
+if (modeOutside == dark) {
+    darkMode();
+    console.log('dark mode enabled')
+}
+
+// header.classList.contains(dark) != true  
 toggle.addEventListener('click', () => {
-    if (header.classList.contains(dark) != true) {
-        toggler.classList.add('animation1');
-        toggler.classList.replace('animation2', 'animation1');
-        header.classList.add(dark);
-        socialMedia.classList.add(dark);
-        overView.classList.add(dark);
-        body.style.background = 'hsl(230, 17%, 14%)';
+    let mode = localStorage.getItem('mode');
+    if (mode != dark) {
+        darkMode();
+        localStorage.setItem('mode', dark);
         console.log('dark mode added');
     } else {
-        toggler.classList.replace('animation1', 'animation2')
-        header.classList.remove(dark);
-        socialMedia.classList.remove(dark);
-        overView.classList.remove(dark);
-        body.style.background = '#fff   ';
+        lightMode();
+        localStorage.setItem('mode', light);
         console.log('dark mode removed');
     }
 })
 
-// header.classList.add(dark);
-// socialMedia.classList.add(dark);
-// overView.classList.add(dark);
-// body.style.background = 'hsl(230, 17%, 14%)';
-// console.log(toggle);
+function darkMode() {
+    toggler.classList.add('animation1');
+    toggler.classList.replace('animation2', 'animation1');
+    header.classList.add(dark);
+    socialMedia.classList.add(dark);
+    overView.classList.add(dark);
+    body.style.background = 'hsl(230, 17%, 14%)';
+}
+function lightMode() {
+    toggler.classList.replace('animation1', 'animation2')
+    header.classList.remove(dark);
+    socialMedia.classList.remove(dark);
+    overView.classList.remove(dark);
+    body.style.background = '#fff   ';
+}
