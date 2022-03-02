f7c60f484e051191d6fbf9ec6e82d8d29da7a471
Alone007 <email_address>
2022-03-02 14:03:39 +0530

    not yet fully finished

--- script.js
+++ script.js
@@ -61,6 +61,76 @@ function Calculator() {
     let equal = document.querySelector('.equal');
     let operation = document.querySelectorAll('[data-operation]');
     let screenText = document.querySelector('.screen_text');
+    let oper;
+
+    // reset button
+    reset.addEventListener('click', _ => {
+        screenText.innerText = '';
+    })
+
+    // delete button
+    del.addEventListener('click', _ => {
+        screenText.innerText = String(screenText.innerText).slice(0, -1);
+    })
+
+    numbers.forEach(button => {
+        button.addEventListener('click', _ => {
+            if ((screenText.innerText).includes('.') && button.innerText == '.') return;
+            screenText.innerText += String(button.innerText);
+        })
+    })
+    operation.forEach(button => {
+        button.addEventListener('click', _ => {
+            let operand = button.dataset.operation;
+            let arr = ['+', '-', '*', '/'];
+            // MUST WORK NOT FINISHED YET
+            // if (String(screenText.innerText).includes(operand)) return console.log('dont duplicate') // TO CHECK ANOTHER REPLICATION
+            for (let sus of arr) {
+                if (String(screenText.innerText).includes(sus)) { compute() };
+            }
+            screenText.innerText += String(operand);
+            oper = String(operand);
+            // console.log(search)
+        })
+    })
+
+    equal.addEventListener('click', compute);
+
+    function compute() {
+        let arr = String(screenText.innerText).split('');
+        let match = String(screenText.innerText).search(/[+*-/]/g);
+        let before = String(screenText.innerText).slice(0, match);
+        console.log('before ' + before);
+        let beforeNum = parseFloat(before);
+        let after = String(screenText.innerText).slice(match + 1);
+        console.log('after ' + after);
+        let afterNum = parseFloat(after);
+        let total;
+
+        if (match == false) return console.log('failed');
+        if (before == '' || after == '') return;
+        switch (oper) {
+            case '+': {
+                total = (beforeNum + afterNum);
+            }
+                break;
+            case '-': {
+                total = (beforeNum - afterNum);
+            }
+                break;
+            case '*': {
+                total = (beforeNum * afterNum);
+            }
+                break;
+            case '/': {
+                total = (beforeNum / afterNum);
+            }
+                break;
+            default: console.log('nothing');
+        }
+
+        return screenText.innerText = total;
+    }
 
 }
 
